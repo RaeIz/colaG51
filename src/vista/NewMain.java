17b5ef7bdceae6a4c8874a08238c1617ce6abb34
juan182 <60664888+juan182@users.noreply.github.com>
2023-08-21 13:18:06 -0500

    Cambio loop 1
    Cambie un poco el loop, en vista estan las notas del cambio

--- src/vista/NewMain.java
+++ src/vista/NewMain.java
@@ -18,24 +18,29 @@ public class NewMain {
     /**
      * @param args the command line arguments
      */
+    //Si creo un static por fuera y lo relleno en el loop deberia funcionar para crear variables infinitas 
+    static void print(String value) {
+        System.out.println(value);
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Cola<Carro> colaCarros=new Cola<>();
         
-        boolean t = true;
-
-        //formula para generar valores aleatorios con un maximo y minimo 
-        //int b = (int)(Math.random()*(max-min+1)+min);
-        do{
+        //Loop
+        while(true){
+            //formula para generar valores aleatorios con un maximo y minimo 
+            //int b = (int)(Math.random()*(max-min+1)+min);
             int elModelo = (int)(Math.random()*(2024-2000+1)+2000);
+            //por ahora el nombre estara en blanco para confirmar que el loop funciona 
             String elNombre = "";
             int elTiempoLan = (int)(Math.random()*(5-1+1)+1);
             
             colaCarros.encolar(new Carro(elModelo,elNombre, elTiempoLan));
             
-            System.out.println(colaCarros.toString());
-        }while(t = true);
-        
+            print(colaCarros.toString());
+            //System.out.println(colaCarros.toString());
+        }  
     }
     
 }
